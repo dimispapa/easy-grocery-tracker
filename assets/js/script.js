852602de706ef4f6e86eb68d56c506bc662ab501
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-05-19 21:28:30 +0000

    feat: add validation for duplicate items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
-// define function to validate inputs
-function validateUserInput(userInput) {
+// define function to check for duplicate list item
+function validateIfDuplicate(userInput, ulElement) {
+  // get array of li elements in ul
+  let items = ulElement.getElementsByTagName('li');
+  // check if ul has at least one li element
+  if (items.length !== 0) {
+    // loop through li elements to check if new text input exists
+    for (let item of items) {
+      // check for a case insesitive match
+      if (item.textContent.toLowerCase() === userInput.toLowerCase()) {
+        alert('This item already exists in the list!');
+        return false;
+      }
+    }
+  }
+  // default return as true if no duplicate found in loop
+  return true;
+};
+
+// define function to validate if blank
+function validateIfBlank(userInput) {
+  // if input is empty string/blank
   if (userInput == "") {
-    alert("Item must be filled out")
+    alert("Item must be filled out!")
     return false;
   } else {
     return true;
@@ -11,15 +31,15 @@ function validateUserInput(userInput) {
 // define function for adding new items
 function addItem(event) {
 
-  // Get the button that triggered the event
+  // Get the button that triggered the event and the ul element next to it
   let button = event.target;
+  let ul = button.nextElementSibling;
 
-  // get the input element and user entry
-  let userInput = button.previousElementSibling.value
-  if (validateUserInput(userInput)) {
+  // get the user input text and apply trim method to ensure no spaces
+  let userInput = button.previousElementSibling.value.trim();
 
-    // Get the next sibling element (the ul element)
-    let ul = button.nextElementSibling;
+  // Validate input
+  if (validateIfBlank(userInput) && validateIfDuplicate(userInput, ul)) {
 
     // add li item with user entry text
     let newLi = document.createElement('li');
