d0972a5f0a36d81e70f0795d2299d57bed4058ae
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-05-19 20:08:48 +0000

    feat: add code to get user input text

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,20 @@
 function addItem(event) {
   console.log('You clicked the add-item button.')
   console.log(event.id);
+
   // Get the button that triggered the event
   let button = event.target;
   console.log('Button ID:', button.id);
 
+  // get the input element and user entry
+  let input = button.previousElementSibling;
+  console.log('User entry:', input.value);
+
   // Get the next sibling element (the ul element)
   let ul = button.nextElementSibling;
   console.log('Next Element:', ul);
+
+  // add li item
 };
 
 // get an array of buttons
