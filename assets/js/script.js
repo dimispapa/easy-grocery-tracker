f77b9bb808e80a1ae24cb5521ef1e4519548e3be
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-05-19 20:41:27 +0000

    feat: add validation of input for empty text
    also remove console.logs once certain that code operates as expected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,31 @@
+// define function to validate inputs
+function validateUserInput(userInput) {
+  if (userInput == "") {
+    alert("Item must be filled out")
+    return false;
+  } else {
+    return true;
+  }
+};
+
 // define function for adding new items
 function addItem(event) {
-  console.log('You clicked the add-item button.')
-  console.log(event.id);
 
   // Get the button that triggered the event
   let button = event.target;
-  console.log('Button ID:', button.id);
 
   // get the input element and user entry
-  let input = button.previousElementSibling;
-  let userInput = input.value;
-  console.log('User entry:', userInput);
+  let userInput = button.previousElementSibling.value
+  if (validateUserInput(userInput)) {
 
-  // Get the next sibling element (the ul element)
-  let ul = button.nextElementSibling;
-  console.log('Next Element:', ul);
+    // Get the next sibling element (the ul element)
+    let ul = button.nextElementSibling;
 
-
-  // add li item with user entry text
-  let newLi = document.createElement('li');
-  newLi.textContent = userInput;
-  ul.appendChild(newLi);
-  console.log('New list:', ul.innerHTML);
+    // add li item with user entry text
+    let newLi = document.createElement('li');
+    newLi.textContent = userInput;
+    ul.appendChild(newLi);
+  };
 
 };
 
