a29ebc3bc40c6c97cbbff717b44677e9b9f96b6f
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-05-24 15:09:12 +0000

    refactor: split javascript files, remove from html

---
+++ assets/js/firebase.js
@@ -0,0 +1,35 @@
+  // initialize firebase
+  import {
+    initializeApp
+  } from 'https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js'
+
+  // add the Firebase SDK for Google Analytics
+  import {
+    getAnalytics
+  } from 'https://www.gstatic.com/firebasejs/10.12.1/firebase-analytics.js'
+
+  // Add Firebase products
+  import {
+    getDatabase,
+    ref,
+    set
+  } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-database.js"
+
+  // Project configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBFDjhdHJ3_I2GYxgeJ-EFBlomj3Oxd_xo",
+    authDomain: "easy-grocery-tracker.firebaseapp.com",
+    projectId: "easy-grocery-tracker",
+    storageBucket: "easy-grocery-tracker.appspot.com",
+    messagingSenderId: "442203934572",
+    appId: "1:442203934572:web:8dcefeebdc8d4b9a09ed49",
+    measurementId: "G-XQMGRZ6KG0",
+    databaseURL: "https://easy-grocery-tracker-default-rtdb.europe-west1.firebasedatabase.app/"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Realtime Database and get a reference to the service
+  const database = getDatabase(app);
